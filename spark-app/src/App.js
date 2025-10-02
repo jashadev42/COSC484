4e307f956a474750fddee0f109ea43d4dece84e2
Nate <natewbrooks@gmail.com>
2025-10-02 13:38:10 -0400

    Init React app + tailwind install

---
+++ spark-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav className='bg-yellow-400'>
+        <h1>spark.</h1>
+      </nav>
+      <content>
+        app content
+      </content>
+    </div>
+  );
+}
+
+export default App;
