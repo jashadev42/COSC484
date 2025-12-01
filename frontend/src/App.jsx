b890b836ac9f7508bad5badebb507605c536eed6
Jasha Phillips <jasha.k.phillips@gmail.com>
2025-12-01 13:33:07 -0500

    New front end structure. Login screen, onboarding, spark view, chat view, settings view

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,12 @@
-import { useState } from 'react'
-import TitleBarComponent from './components/TitleBarComponent'
-import PhoneAuthFormComponent from './components/PhoneAuthFormComponent'
-import './index.css'
+// src/App.jsx
+import React from "react";
+import AppRoutes from "./routes/AppRoutes";
+import "./index.css";
 
 export default function App() {
-    return (
-        <section className='flex flex-col w-full h-full'>
-            <header>
-                <TitleBarComponent/>
-            </header>
-            <main className='flex w-full justify-center '>
-                <PhoneAuthFormComponent/>
-            </main>
-            <footer>
-            </footer>
-        </section>
-    )
+  return (
+    <div className="w-full min-h-screen">
+      <AppRoutes />
+    </div>
+  );
 }
