fa9c00bf749005c865d49aeaa24caee559788bd1
ARLS4396 <154839522+ARLS4396@users.noreply.github.com>
2025-12-02 22:32:00 -0500

    Implement conditional rendering of ProfileScreen based on authentication status

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
 import TitleBarComponent from './components/TitleBarComponent'
 import PhoneAuthFormComponent from './components/PhoneAuthFormComponent'
+import ProfileScreen from './components/ProfileScreen'
+import { useAuth } from '@contexts/AuthContext'
 import './index.css'
 
 export default function App() {
+    const { isAuthenticated } = useAuth()
+
     return (
         <section className='flex flex-col w-full h-full'>
             <header>
                 <TitleBarComponent/>
             </header>
             <main className='flex w-full justify-center '>
-                <PhoneAuthFormComponent/>
+                {isAuthenticated ? <ProfileScreen/> : <PhoneAuthFormComponent/>}
             </main>
             <footer>
             </footer>
