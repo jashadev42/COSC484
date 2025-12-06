d71de28de5bcc6788c79618f9a07909ea1639a16
Nate <natewbrooks@gmail.com>
2025-12-05 20:21:21 -0500

    Change frontend API URL to match hosted backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   define: {
-    '__API_URL__': JSON.stringify("http://localhost:8000")
+    '__API_URL__': JSON.stringify("https://spark-dating-api.vercel.app")
   },
   resolve: {
     alias: {
