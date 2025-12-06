a7fd643a54a8aac4696662b32eb5696d03aef58e
Nate <natewbrooks@gmail.com>
2025-12-06 18:32:35 -0500

    Add prototype of matchmaking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-import TitleBarComponent from './components/TitleBarComponent'
-import PhoneAuthFormComponent from './components/PhoneAuthFormComponent'
+import TitleBarComponent from '@components/TitleBarComponent'
+import PhoneAuthFormComponent from '@components/PhoneAuthFormComponent'
 import './index.css'
+import { useAuth } from '@contexts/AuthContext'
+import { Matchmaking } from './components/Matchmaking'
 
 export default function App() {
+    const {isAuthenticated} = useAuth();
+
     return (
         <section className='flex flex-col w-full h-full'>
             <header>
                 <TitleBarComponent/>
             </header>
+            {isAuthenticated && <Matchmaking/>}
             <main className='flex w-full justify-center '>
                 <PhoneAuthFormComponent/>
             </main>
